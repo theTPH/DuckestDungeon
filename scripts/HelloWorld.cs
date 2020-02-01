37156f5ede8fde7c5ae380de9f5e4caabb506b32
mkuehne97 <mkuehne8@gmail.com>
2020-02-01 16:47:14 +0100

    many-to-many relationship added

--- scripts/HelloWorld.cs
+++ scripts/HelloWorld.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 
+using System.Collections.Generic;
+
 using NHibernate.Cfg;
 using NHibernate;
 using NHibernate.Tool.hbm2ddl;
@@ -9,81 +11,97 @@ using NHibernate.Tool.hbm2ddl;
 
 namespace TestProgram
 {
-    public class HelloWorld : RichTextLabel
-    {
-        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        log4net.ILog log;
+	public class HelloWorld : RichTextLabel
+	{
+		//private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		log4net.ILog log;
 
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
-        {
-            try
-            {
-                log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-                log.Info("Hallo erstmal.");
-                ISession session = initHibernate();
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			try
+			{
+				log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+				log.Info("Hallo erstmal.");
+				ISession session = initHibernate();
 
-                GD.Print("Hello World!!");
-                this.Text = "Hello World";
+				GD.Print("Hello World!!");
+				this.Text = "Hello World";
 
-                var Product = new Product
-                {
-                    Name = "Produkt Name",
-                    Category = 1,
-                    Discontinued = false,
+				var Product = new Product
+				{
+					Name = "Produkt Name",
+					Category = 1,
+					Discontinued = false,
 
-                    Price = new Price
-                    {
-                        min = 0,
-                        max = 100,
-                        average = 50
-                    }
-                };
+					Price = new Price
+					{
+						min = 0,
+						max = 100,
+						average = 50
+					}
+				};
 
-                session.Save(Product);
-                session.Flush();
+				session.Save(Product.Price);
+				session.Save(Product);
+				session.Flush();
 
-                }
-                catch (Exception e)
-                {
-                    log.Error("Fehler bei der codeausführung.", e);
-                }
-        }
+				}
+				catch (Exception e)
+				{
+					log.Error("Fehler bei der codeausführung.", e);
+				}
+		}
 
-        ISession initHibernate()
-        {
-            // read hibernate.cfg.xml, initializing hibernate
-            var cfg = new Configuration();
-            cfg.Configure("config/hibernate.cfg.xml");
+		ISession initHibernate()
+		{
+			// read hibernate.cfg.xml, initializing hibernate
+			var cfg = new Configuration();
+			cfg.Configure("config/hibernate.cfg.xml");
 
-            //cfg.AddAssembly(typeof(Product).Assembly);
-            //cfg.AddAssembly(typeof(Price).Assembly);
-            cfg.AddFile("config/mappings/Price.hbm.xml");
-            cfg.AddFile("config/mappings/Product.hbm.xml");
-            
-            // Create Tables in DB
-            new SchemaExport(cfg).Create(false, true);
+			//cfg.AddAssembly(typeof(Product).Assembly);
+			//cfg.AddAssembly(typeof(Price).Assembly);
+			//cfg.AddFile("config/mappings/Price.hbm.xml");
+			//cfg.AddFile("config/mappings/Product.hbm.xml");
 
-            // Get ourselves an NHibernate Session
-            var sessions = cfg.BuildSessionFactory();
-            return sessions.OpenSession();
-        }
-    }
+			IEnumerable<string> Files = System.IO.Directory
+				.EnumerateFiles("config/mappings", "*.*", System.IO.SearchOption.TopDirectoryOnly);
+			
+			foreach(string File in Files)
+				if (File.EndsWith(".hbm.xml"))
+					cfg.AddFile(File);
 
-    public class Product
-    {
-        public virtual int Id { get; set; }
-        public virtual string Name { get; set; }
-        public virtual int Category { get; set; }
-        public virtual bool Discontinued { get; set; }
-        public virtual Price Price { get; set; }
-    }
+			// Create Tables in DB
+			new SchemaExport(cfg).Create(false, true);
 
-    public class Price
-    {
-        public virtual int Id { get; set; }
-        public virtual int min { get; set; }
-        public virtual int max { get; set; }
-        public virtual int average { get; set; }
-    }
+			// Get ourselves an NHibernate Session
+			var sessions = cfg.BuildSessionFactory();
+			return sessions.OpenSession();
+		}
+	}
+
+	public class Product
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual int Category { get; set; }
+		public virtual bool Discontinued { get; set; }
+		public virtual Price Price { get; set; }
+		public virtual IList<Store> Stores { get; set; }
+	}
+
+	public class Price
+	{
+		public virtual int Id { get; set; }
+		public virtual int min { get; set; }
+		public virtual int max { get; set; }
+		public virtual int average { get; set; }
+	}
+
+	public class Store
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual IList<Product> Products { get; set; }
+	}
 }
